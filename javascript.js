7bef169d0ada32dc237c20e3e0561615d599ebe3
essenJean223 <76797109+essenJean223@users.noreply.github.com>
2021-09-26 13:08:03 -0500

    Add files via upload

--- javascript.js
+++ javascript.js
@@ -8,13 +8,13 @@ window.addEventListener('load', () => {
     imagenes[2] = 'img/tecno.jpg';
     imagenes[3] = 'img/teledyne.jpg';
     imagenes[4] = 'img/Kal.PNG';
-    imagenes[5] = 'img/andros.png';
+    imagenes[5] = 'img/andros.PNG';
     imagenes[6] = 'img/sensors.jpg';
     imagenes[7] = 'img/envitec.png';
     imagenes[8] = 'img/Citi.png';
     imagenes[9] = 'img/tele-sensor.png';
-    imagenes[10] = 'img/andros-62xx.png';
-    imagenes[11] = 'img/andros-6500.png';
+    imagenes[10] = 'img/andros-62xx.PNG';
+    imagenes[11] = 'img/andros-6500.PNG';
 
     function cambiarImagenes() {
         if (contador < imagenes.length) {
